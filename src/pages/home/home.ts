181f7ccc0719a4bd9a0d0cb425808c616939b35e
ClémentM <clement.marescaux@gmail.com>
2018-05-17 16:22:48 +0200

    Page à propos created

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
+
 
 @Component({
   selector: 'page-home',
@@ -9,6 +11,13 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToAbout() {
+
+    // On change la page d'accueil par la page à propos 
+    // au clic sur le bouton
+    this.navCtrl.push(AboutPage);
   }
 
 }
